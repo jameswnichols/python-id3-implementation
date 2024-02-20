b0e7196e13d24b21464ff1332aeafaba3f5418af
James Nichols <jnichols.tigley@gmail.com>
2024-02-20 16:11:59 +0000

    Can view block layout of nodes.

--- renderNodes.py
+++ renderNodes.py
@@ -5,6 +5,9 @@ import pickle
 
 SCREEN_SIZE = Vector2(800, 600)
 
+DISTRIBUTION_WIDTH = 600
+DISTRIBUTION_HEIGHT_GAP = 150
+
 class Object:
     def __init__(self, position : Vector2, renderSurface : pygame.Surface):
         self.position = position
@@ -15,11 +18,28 @@ class Object:
         self.renderRect.topleft = self.position
         return screenRect.colliderect(self.renderRect)
 
-screen = pygame.display.set_mode(SCREEN_SIZE)
+def generateObjectsFromNodes(nodes : dict[Node], rootNodeName : str):
+    nodesToAdd = [{"node":nodes[rootNodeName],"position":Vector2(0, 0)}]
+    objects = []
+    while len(nodesToAdd) > 0:
+        poppedNode = nodesToAdd.pop(0)
+        nodeToAdd, nodePosition = poppedNode["node"], poppedNode["position"]
+        childNodeStartPosition = nodePosition - Vector2(DISTRIBUTION_WIDTH/2, 0)
+        childDistanceBetween = DISTRIBUTION_WIDTH / (len(nodeToAdd.children) + 1)
+        for i, child in enumerate(nodeToAdd.children.values()):
+            childPosition = childNodeStartPosition + Vector2(childDistanceBetween * (i + 1), DISTRIBUTION_HEIGHT_GAP)
+            nodesToAdd.append({"node":child,"position":childPosition})
+        objects.append(Object(nodePosition, blankSurface))
+    return objects
+        
+screen = pygame.display.set_mode(SCREEN_SIZE, vsync=True)
 running = True
 
 cameraPosition = Vector2(0, 0)
 cameraRect = pygame.Rect(cameraPosition, SCREEN_SIZE)
+cameraHeld = False
+mouseHoldStartPosition = Vector2(0, 0)
+cameraHoldStartPosition = Vector2(0, 0)
 
 nodes = None
 with open("nodesOutput.data", "rb") as f:
@@ -28,10 +48,12 @@ with open("nodesOutput.data", "rb") as f:
 blankSurface = pygame.Surface((50, 50))
 blankSurface.fill((255,0,0))
 
-objects = [Object(Vector2(0, 0), blankSurface)]
+objects = generateObjectsFromNodes(nodes, "Root")#[Object(Vector2(0, 0), blankSurface)]
 
 while running:
     screen.fill((255,255,255))
+
+    cameraRect.topleft = cameraPosition
 
     for object in objects:
         if not object.isVisible(cameraRect):
@@ -43,6 +65,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    if pygame.mouse.get_pressed()[0] and not cameraHeld:
+        cameraHeld = True
+        mouseHoldStartPosition = pygame.mouse.get_pos()
+        cameraHoldStartPosition = cameraPosition
+    elif pygame.mouse.get_pressed()[0] and cameraHeld:
+        cameraPosition = cameraHoldStartPosition - (Vector2(pygame.mouse.get_pos())-mouseHoldStartPosition)
+    elif not pygame.mouse.get_pressed()[0] and cameraHeld:
+        cameraHeld = False
+
     pygame.display.flip()
 
 pygame.quit()
