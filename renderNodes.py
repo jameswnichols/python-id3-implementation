3dbee718fb92d2fef775b22c7a6ba70ef0c48943
James Nichols <jnichols.tigley@gmail.com>
2024-02-21 11:29:33 +0000

    Renders text now.

--- renderNodes.py
+++ renderNodes.py
@@ -3,37 +3,75 @@ from pygame import Vector2
 from mainRewrite import *
 import pickle
 
+pygame.init()
+
+FONT = [pygame.font.SysFont("arialrounded", x) for x in range(1, 100)]
+
 SCREEN_SIZE = Vector2(800, 600)
 
-DISTRIBUTION_WIDTH = 600
+DISTRIBUTION_WIDTH = 400
 DISTRIBUTION_HEIGHT_GAP = 150
 
 class Object:
-    def __init__(self, position : Vector2, renderSurface : pygame.Surface):
+    def __init__(self, position : Vector2, renderSurface : pygame.Surface, nodeClass : str):
         self.position = position
         self.renderSurface = renderSurface
         self.renderRect = renderSurface.get_rect()
+        self.renderRect.topleft = self.position
+
+        self.fontSurface = FONT[12].render(nodeClass, True, (0, 0, 255))
+        self.fontSurfacePosition = self.renderRect.center - Vector2(self.fontSurface.get_rect().center)
 
     def isVisible(self, screenRect : pygame.Rect):
         self.renderRect.topleft = self.position
         return screenRect.colliderect(self.renderRect)
 
+    def render(self, screen : pygame.Surface, screenRect : pygame.Rect):
+        screenPosition = self.position - screenRect.topleft
+        screen.blit(self.renderSurface, screenPosition)
+        screen.blit(self.fontSurface, self.fontSurfacePosition-screenRect.topleft)
+    
+class Line:
+    def __init__(self, startPosition : Vector2, endPosition : Vector2, text : str):
+        self.startPosition = startPosition
+        self.endPosition = endPosition
+
+        self.fontSurface = FONT[12].render(text, True, (0, 0, 255))
+        fontLinePosition = self.startPosition.lerp(self.endPosition, 0.5)
+        self.fontSurfacePosition = fontLinePosition - Vector2(self.fontSurface.get_rect().center)
+
+        self.renderRect = pygame.Rect(self.startPosition, (self.endPosition + Vector2(1, 1)) - self.startPosition)
+    
+    def isVisible(self, screenRect : pygame.Rect):
+        self.renderRect.topleft = self.startPosition
+        return screenRect.colliderect(self.renderRect)
+
+    def render(self, screen : pygame.Surface, screenRect : pygame.Rect):
+        screenStartPosition = self.startPosition - screenRect.topleft
+        screenEndPosition = self.endPosition - screenRect.topleft
+        pygame.draw.line(screen, (155,155,155), screenStartPosition, screenEndPosition,3)
+        screen.blit(self.fontSurface, self.fontSurfacePosition - screenRect.topleft)
+
 def generateObjectsFromNodes(nodes : dict[Node], rootNodeName : str):
     nodesToAdd = [{"node":nodes[rootNodeName],"position":Vector2(0, 0)}]
     objects = []
-    while len(nodesToAdd) > 0:
+    lines = []
+    while len(nodesToAdd) > 0 :
         poppedNode = nodesToAdd.pop(0)
         nodeToAdd, nodePosition = poppedNode["node"], poppedNode["position"]
         childNodeStartPosition = nodePosition - Vector2(DISTRIBUTION_WIDTH/2, 0)
         childDistanceBetween = DISTRIBUTION_WIDTH / (len(nodeToAdd.children) + 1)
-        for i, child in enumerate(nodeToAdd.children.values()):
+        for i, (joiningValue, child) in enumerate(nodeToAdd.children.items()):
             childPosition = childNodeStartPosition + Vector2(childDistanceBetween * (i + 1), DISTRIBUTION_HEIGHT_GAP)
+            lines.append(Line(nodePosition + Vector2(12.5, 12.5), childPosition + Vector2(12.5, 12.5),joiningValue))
             nodesToAdd.append({"node":child,"position":childPosition})
-        objects.append(Object(nodePosition, blankSurface))
-    return objects
+        
+        objects.append(Object(nodePosition, blankSurface, nodeToAdd.paramClass))
+    return lines, objects
         
 screen = pygame.display.set_mode(SCREEN_SIZE, vsync=True)
 running = True
+
 
 cameraPosition = Vector2(0, 0)
 cameraRect = pygame.Rect(cameraPosition, SCREEN_SIZE)
@@ -45,21 +83,27 @@ nodes = None
 with open("nodesOutput.data", "rb") as f:
     nodes = pickle.load(f)
 
-blankSurface = pygame.Surface((50, 50))
+blankSurface = pygame.Surface((25, 25))
 blankSurface.fill((255,0,0))
 
-objects = generateObjectsFromNodes(nodes, "Root")#[Object(Vector2(0, 0), blankSurface)]
+lines, objects = generateObjectsFromNodes(nodes, "Root")#[Object(Vector2(0, 0), blankSurface)]
 
 while running:
     screen.fill((255,255,255))
 
     cameraRect.topleft = cameraPosition
 
+    for line in lines:
+        if not line.isVisible(cameraRect):
+            continue
+        
+        line.render(screen, cameraRect)
+
     for object in objects:
         if not object.isVisible(cameraRect):
             continue
-        objectPosition = object.position - cameraPosition
-        screen.blit(object.renderSurface, objectPosition)
+
+        object.render(screen, cameraRect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
