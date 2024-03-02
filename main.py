4d0200a0ef95a883e69c4d169f40fc242585864b
James Nichols <jnichols.tigley@gmail.com>
2024-03-02 17:32:55 +0000

    SO MANY COMMENTS.

--- main.py
+++ main.py
@@ -18,19 +18,24 @@ class Node:
 
 def calculateClassValueEntropysFromDataset(paramClasses : list[str], dataset : list[dict], rootClass : str, pathItems : list[tuple[str, str]]):
     #Initialise a dictionary where each parameter class value has an inner dictionary with all of the root class' value counts.
-    #E.g. {"boot_space": {"small":{"vgood":0,"good":4,"acc":132,"unacc":375},"med":...}, "safety" : ...}
+    #E.g. {"boot_space": {"small":{"vgood":0,"good":4,"acc":132,"unacc":375},"med":...}, "safety" : ...}.
 
-    #Defaultdict calls a function when the key is not found, so lambda is used to create an inner dictionary with a default value of 0.
+    #Defaultdict calls a function when the key is not found, so lambda is used to create 2 inner dictionaries with a default value of 0.
+    #Defaultdicts are used as it prevents the need to check if the key is in the dictionary before adding a value to it.
+    #E.g. {None : {None : 0, ...}, ...}
     paramRootValues = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
     rootValueCounts = defaultdict(int)
     datasetLength = 0
     for row in dataset:
         rowItems = row.items()
+        #Check if the path is completely contained in the row, with path being key-value pairs to filter by e.g. path={"buying_price":"high","safety":"med",...}.
         if pathItems <= rowItems:
+            #Remove the path from the row.
             newRow = dict(rowItems - pathItems)
             for paramClass in paramClasses:
-            #Increase the count of the parameter class' root class value.            
+                #Increase the count of the parameter class' root class value e.g. {"boot_space": {"small":"vgood"}} += 1.
                 paramRootValues[paramClass][newRow[paramClass]][newRow[rootClass]] += 1
+            #Counts the root class values to calculate the entropy of the root class later.
             rootValueCounts[newRow[rootClass]] += 1
             datasetLength += 1
 
@@ -38,14 +43,16 @@ def calculateClassValueEntropysFromDataset(paramClasses : list[str], dataset : l
     for paramClass, paramRootValueData in paramRootValues.items():
         paramEntropys = {}
         for paramValue, rootValuesDict in paramRootValueData.items():
+            #Is equal to the counts of this paramter class' root class values e.g. [0, 4, 132, 375].
             rootValues = list(rootValuesDict.values())
-            #Calculate the entropy of each of the parameter class' values.
             valTotal = sum(rootValues)
+            #Calculate the entropy of each of the parameter class' values e.g. "small", "med", "big".
             valProbs = [crV / valTotal for crV in rootValues]
             paramEntropy = -sum([prob * math.log2(prob) if prob else 0 for prob in valProbs])
             paramEntropys[paramValue] = {"entropy":paramEntropy,"total":valTotal,"rootValueCounts":rootValuesDict}
-            paramClassEntropys[paramClass] = paramEntropys
+        paramClassEntropys[paramClass] = paramEntropys
     
+    #Calculate the entropy of the root class e.g. "quality".
     rootEntropy = -sum([((rootValueCount / datasetLength) * math.log2(rootValueCount / datasetLength)) if rootValueCount else 0 for rootValueCount in rootValueCounts.values()])
 
     return paramClassEntropys, rootEntropy, datasetLength
@@ -55,19 +62,16 @@ def calculateClassInformationGainFromDataset(classEntropys : dict, datasetLength
     return rootClassEntropy - sum([(cEV["total"]/datasetLength) * cEV["entropy"] for cEV in list(classEntropys.values())])
 
 def getPossibleClassCountsFromDataset(paramClass : str, dataset : list[dict]):
-    possibleClasses = {}
+    #Gets the possible values of a parameter class, and their counts e.g. "quality" = {"vgood": 54, "good":67, "acc":532, "unacc":1268}.
+    possibleClasses = defaultdict(int)
     for row in dataset:
-        if row[paramClass] not in possibleClasses:
-            possibleClasses[row[paramClass]] = 1
-        else:
-            possibleClasses[row[paramClass]] += 1
-        #possibleClasses.append(row[paramClass])
+        possibleClasses[row[paramClass]] += 1
     return possibleClasses
 
 def getLeavesBranchesFromBestClass(bestFittingClass : dict):
     leaves = []
     expand = []
-    #Check if multiple values result in the same thing, if so then group them.
+    #Check the entropy of each value of the best fitting class e.g. "safety" = {"low":{"entropy":-0.0,...}, "med":{"entropy":1.23,...}, ...}.
     for value, entropyData in bestFittingClass["entropys"].items():
         if entropyData["entropy"] == 0.0:
             #If entropy of value is 0, then the value is a leaf, so find only non-zero root value.
@@ -76,30 +80,40 @@ def getLeavesBranchesFromBestClass(bestFittingClass : dict):
                 if rootValueCount > 0:
                     entropyResultValue = rootValue
                     break
-
+            #Value is used so when validating the dataset, it can relate to a specific value of the root class e.g. "low" -> "quality" = "unacc".
             leaves.append({"value":value,"result":entropyResultValue})
         else:
+            #If the entropy isnt 0, then the value is a branch, so dataset will be filtered by the value later on.
             expand.append(value)
     return leaves, expand
 
 def renderNodes(node : Node, indent : int, rootClass : str, parentClass = None, parentClassValue = None):
+    #If is used so that tree can show the previous class value of the node at the current recursion.
+    #E.g. "If Safety = med; Check buying_price:".
     classValueString = f"If {parentClass} = {parentClassValue}; " if parentClassValue and parentClass else ""
     print("  " * (indent - 1) + f"{classValueString}Check {node.Class}:")
     for classValue, result in node.decisions.items():
+        #E.g. "If safety = low; quality = unacc".
         print("  " * (indent) + f"If {node.Class} = {classValue}; {rootClass} = {result}")
     for pathValue, childNode in node.children.items():
+        #Indent is used so that the child nodes are indented under the parent node, looks like "branches".
         renderNodes(childNode, indent + 1, rootClass,node.Class ,pathValue)
 
-def getResultOfDatasetEntry(entry : dict, startingNode : Node):
+def getResultOfDatasetEntry(row : dict, startingNode : Node):
+    #Instead of using recursion, uses a queue to traverse child nodes to find the tree's predicted result of a row.
     nodesToCheck = [startingNode]
     while len(nodesToCheck) > 0:
+        #Get the next node in the queue.
         nodeToCheck = nodesToCheck.pop(0)
-        entryValue = entry[nodeToCheck.Class]
+        #Get the value of the node's class in the row e.g. row["safety"] = "low".
+        entryValue = row[nodeToCheck.Class]
+        #If the of the node's class is a leaf, then the value of the leaf is predicted result e.g. "safety" = "low" -> "quality" = "unacc".
         if entryValue in nodeToCheck.decisions:
             return nodeToCheck.decisions[entryValue]
+        #If the node's class is a branch, check that branches' node next e.g. "safety" = "med" -> "buying_price" Node.
         elif entryValue in nodeToCheck.children:
             nodesToCheck.append(nodeToCheck.children[entryValue])
-
+    #Fallback in case the tree doesnt have a value for that row. Unacc is 70% of the dataset so it is the default result.
     return "unacc"
 
 def getNodesFromDataset(dataset : list[dict], classToCheck : str):
@@ -126,11 +140,11 @@ def getNodesFromDataset(dataset : list[dict], classToCheck : str):
 
         classesInDataset = set(dataset[0].keys() - pathToCheck.keys())
         classesInDataset.remove(classToCheck)
-
-        bestFittingClass = {"infoGain":-100.0}
-                #By only reading the dataset rows once per while loop iteration, triples the speed of the algorithm.
+        
+        #By only reading the dataset rows once per while loop iteration, triples the speed of the algorithm.
         paramClassEntropys, mainClassCheckEntropy, datasetLength = calculateClassValueEntropysFromDataset(classesInDataset, dataset, classToCheck, pathItems)
 
+        bestFittingClass = {"infoGain":-100.0}
         for paramClass, classEntropys in paramClassEntropys.items():
             classInformationGain = calculateClassInformationGainFromDataset(classEntropys, datasetLength, mainClassCheckEntropy)
             if classInformationGain > bestFittingClass["infoGain"]:
@@ -170,12 +184,12 @@ def extractDatasetFromCSV(filename):
     for i, line in enumerate(fileData[1:]):
         print(" "*len(lastLine),end="\r")
         elapsedTime = time.time() - startTime
-        lastLine = f"Time Elapsed: {round(elapsedTime,2)}s // Lines Processed: {i} / {inputLength} // Percentage Complete: {round((i/inputLength)*100,2)}%"
+        lastLine = f"Time Elapsed: {round(elapsedTime,2)}s // Lines Processed: {i+1} / {inputLength} // Percentage Complete: {round((i/inputLength)*100,2)}%"
         print(lastLine,end="\r")
         dataset.append(dict(zip(classes, line.strip().split(","))))
     print(" "*len(lastLine),end="\r")
     elapsedTime = time.time() - startTime
-    lastLine = f"Time Elapsed: {round(elapsedTime,2)}s // Lines Processed: {i} / {inputLength} // Percentage Complete: {round((i/inputLength)*100,2)}%"
+    lastLine = f"Time Elapsed: {round(elapsedTime,2)}s // Lines Processed: {i+1} / {inputLength} // Percentage Complete: {round((i/inputLength)*100,2)}%"
     print(lastLine,end="\r")
     print("\n")
     return dataset
@@ -221,11 +235,11 @@ def testFindBestTree(dataset : list[dict], rootClass : str, rootClassCounts : li
         totalNodes, rootNode = getNodesFromDataset(trainingDataset, rootClass)
         valid, total = validateDataset(testingDataset, rootNode, rootClass)
         efficiency = ((valid/total) * 100) / math.log2(totalNodes)
-        if efficiency > bestTree["efficiency"] and valid / total > 0.8:
+        if efficiency > bestTree["efficiency"] and valid / total > 0.7:
             bestTree = {"percentage":valid/total, "rootNode":rootNode, "totalNodes":totalNodes, "efficiency":efficiency}
         print(f"({run+1} / {runs}) Valid: {valid}/{total} ({round((valid/total)*100,2)}%)")
     elapsedTime = time.time() - startTime
-    print(f"Best result of {runs} runs in {round(elapsedTime, 2)}s with {round((trainingSetPercentage*100) if trainingSetPercentage else 100, 2)}% of the dataset was {round(bestTree['percentage']*100,2)}% valid with an efficiency of {round(bestTree['efficiency'],2)}% and with {bestTree['totalNodes']} nodes, rendered below:")
+    print(f"Best result of {runs} runs in {round(elapsedTime, 2)}s with {round((trainingSetPercentage*100) if trainingSetPercentage else 100, 2)}% of the dataset was {round(bestTree['percentage']*100,2)}% valid with an efficiency of {round(bestTree['efficiency'],2)}% and with {bestTree['totalNodes']} nodes is rendered below:")
     renderNodes(bestTree["rootNode"], 1, rootClass)
     # with open("bestTreeOutput.data", "wb") as f:
     #     pickle.dump({"Node":bestTree["rootNode"]}, f)
@@ -234,4 +248,4 @@ if __name__ == "__main__":
     loadedDataset = extractDatasetFromCSV("courseworkDataset.csv")
     rootClass = list(loadedDataset[0].keys())[-1]
     rootClassCounts = getPossibleClassCountsFromDataset(rootClass, loadedDataset)
-    testFindBestTree(dataset=loadedDataset, rootClass=rootClass, rootClassCounts=rootClassCounts, runs=30000, trainingSetPercentage=0.03)
+    testFindBestTree(dataset=loadedDataset, rootClass=rootClass, rootClassCounts=rootClassCounts, runs=1500, trainingSetPercentage=None)
